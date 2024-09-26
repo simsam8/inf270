a0d44b90ef0b2d4d1b6d522c11bdbce6e64b0058
simsam8 <simvedaa@gmail.com>
2024-09-26 11:39:07 +0200

    show dual and slack values

--- assignment1/planning.py
+++ assignment1/planning.py
@@ -1,11 +1,35 @@
 from pulp import LpMaximize, LpProblem, LpStatus, LpVariable, lpSum
 
+# with open("small.dat", "r") as f:
+#
+#     content = f.readlines()
+#     n_crudes = int(content[1].strip())
+#     # print(f"Number of crudes: {n_crudes}")
+#     crude_costs = [int(cost) for cost in content[3].strip().split(" ")]
+#     # print(f"Crude costs {crude_costs}")
+#     n_products = int(content[5].strip())
+#     # print(f"Number of end products: {n_products}")
+#     product_price = [int(price) for price in content[7].strip().split(" ")]
+#     # print(f"Product market prices: {product_price}")
+#     crude_yields = []
+#     for index in range(9, 9 + n_crudes):
+#         crude_yields.append([float(val) for val in content[index].strip().split(" ")])
+#
+#     # print(f"Crude yields: {crude_yields}")
+#
+#     capacity_input = int(content[13].strip())
+#     # print(f"Capacity input: {capacity_input}")
+#     capacity_output = int(content[15].strip())
+#     # print(f"Capacity output: {capacity_output}")
+
 model = LpProblem(name="production_planning", sense=LpMaximize)
 
 # Crude materials
 m0 = LpVariable("m0", lowBound=0)
 m1 = LpVariable("m1", lowBound=0)
 m2 = LpVariable("m2", lowBound=0)
+
+# crudes = []
 
 # End products
 n0 = LpVariable("n0", lowBound=0)
@@ -36,7 +60,4 @@ for var in model.variables():
 
 
 for name, c in model.constraints.items():
-    print(f"{name}: {c.value()}")
-    if name in ["capacity_in", "capacity_out"]:
-        print(f"Highest price for {name} expansion: {c.pi}")
-    # print(f"{name}_dual: {c.pi}, \t slack: {c.slack}")
+    print(f"{name} dual: {c.pi}, \t slack: {c.slack}")
